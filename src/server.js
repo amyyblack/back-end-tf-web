6acd71899a214c9c1ea9a095cd15df465e4aeaa1
amyyblack <ars34@aluno.ifnmg.edu.br>
2025-12-09 12:31:58 +0000

    update CORS headers

--- src/server.js
+++ src/server.js
@@ -10,8 +10,9 @@ const app = express();
 app.use(cors({
   origin: '*',
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Admin-ID', 'X-Admin-PASS']
 }));
+
 
 app.use(express.json());
 
